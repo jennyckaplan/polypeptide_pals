5a3cf69113f22ce9907b651c3dba2698347a9268
Connor Jordan <connor_jordan@brown.edu>
2020-11-12 14:25:26 -0500

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#======================< DOWNLOAD SCRIPT >=====================#
+#                                                              #
+#   Pipeline script to download the relevent datasets.         #
+#   Parses Pytorch datasets and saves using TFDatasets.        #
+#                                                              #
+#               USAGE:    $ sh ./download_data.sh              #
+#                                                              #
+#==============================================================#
+
+# Create new directory in model dir
+mkdir -p ./data
+
+
+
+
