e6256e1781052f64fe19020f234de5642e1c83ae
Stephanie Carrero <Stephanie@Stephanies-MBP.home>
2020-12-01 17:27:44 -0500

    implemented amino acid class predictor using keras layers

---
+++ AminoAcidClassPredictor.py
@@ -0,0 +1,26 @@
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv1D, Dense, Layer, Sequential, LayerNormalization
+
+class AminoAcidClassPredictor(Model):
+
+    def __init__(self,
+                 n_classes: int,
+                 use_conv: bool = True) -> None:
+                 
+        super().__init__()
+
+        if use_conv:
+            self.predict_class = Sequential()
+            self.predict_class.add(LayerNormalization())
+            self.predict_class.add(Conv1D(128, 5, activation='relu', padding='same', use_bias=True))
+            self.predict_class.add(Conv1D(n_classes, 3, activation=None, padding='same', use_bias=True))
+        else:
+            self.predict_class = Sequential()
+            self.predict_class.add(LayerNormalization())
+            self.predict_class.add(Dense(512,activation='relu'))
+            self.predict_class.add(Dense(n_classes,activation=None))
+
+    def call(self, inputs):
+        logits = self.predict_class(inputs['encoder_output'])
+        inputs['sequence_logits'] = logits
+        return inputs
